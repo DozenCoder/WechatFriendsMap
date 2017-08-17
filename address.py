1b89b8d2a40516c95eb82f44fed6690bea043969
superPershing <727342027@qq.com>
2017-08-17 23:18:13 +0800

    add return json

--- address.py
+++ address.py
@@ -1,6 +1,22 @@
+from urllib.request import urlopen, quote
+import json
 import requests
 import itchat
 from itchat.content import *
+
+def getlnglat(address):
+    address = quote(address)
+    url = 'http://api.map.baidu.com/geocoder/v2/'
+    output = 'json'
+    ak = 'Uba11lIVgQGGj2xPZPif4mfRtNXjVhSo'
+    uri = url + '?output=' + output + '&address=' + address + '&ak=' + ak
+    # try:
+    temp = urlopen(uri)
+    temp = json.loads(temp.read().decode())
+    # except:
+    # temp = None
+    # finally:
+    return temp
 
 @itchat.msg_register(itchat.content.TEXT)
 def address_reply(msg):
@@ -15,6 +31,7 @@ def address_reply(msg):
             province = i["Province"]
             address_i = nickname + ':' + province + ' ' + city
             address_list += address_i + '\n'
+            print(getlnglat(city))
         itchat.send(address_list, source)
 
 itchat.auto_login(hotReload=True)
