f7290123e5743ad1fc299cc072bfbb2abd9020c2
Super-Pershing <north.wind@qq.com>
2017-08-18 22:31:49 +0800

    add map

---
+++ README.md
@@ -0,0 +1,2 @@
+`printChatroomMemberName` is a boring homework program that work for USTC Internet of Things Technology course.  
+TODO:`address` works with Echart
--- address.py
+++ address.py
@@ -1,3 +1,5 @@
+import webbrowser
+from collections import Counter
 from urllib.request import urlopen, quote
 import json
 import requests
@@ -5,17 +7,16 @@ import itchat
 from itchat.content import *
 
 def getlnglat(address):
+    '''
+    返回字典形式的城市经纬度和其他信息数据
+    '''
     address = quote(address)
     url = 'http://api.map.baidu.com/geocoder/v2/'
     output = 'json'
     ak = 'Uba11lIVgQGGj2xPZPif4mfRtNXjVhSo'
     uri = url + '?output=' + output + '&address=' + address + '&ak=' + ak
-    # try:
     temp = urlopen(uri)
     temp = json.loads(temp.read().decode())
-    # except:
-    # temp = None
-    # finally:
     return temp
 
 @itchat.msg_register(itchat.content.TEXT)
@@ -25,13 +26,28 @@ def address_reply(msg):
         address = itchat.get_friends(update=False)
         print(type(address))
         address_list = ''
+        address_json = dict()
+        address_city_list = []
         for i in address:
             nickname = i["NickName"]
             city = i["City"]
             province = i["Province"]
             address_i = nickname + ':' + province + ' ' + city
             address_list += address_i + '\n'
-            print(getlnglat(city))
+            city_json = getlnglat(city) #type: <class 'dict'>
+            if city_json['status'] == 0:
+                address_city_list.append(city)
+                print(city_json)
+                address_json[city] = city_json
+            elif city_json['status'] != 0:
+                province_json = getlnglat(province) #type: <class 'dict'>
+                if province_json['status'] == 0:
+                    address_city_list.append(province)
+                    print(province_json)
+                    address_json[province] = province_json
+        
+        webbrowser.open('/home/tiger/Workspace/itChatAttendanceCheck/addressMap/index.html')
+        print(address_json)
         itchat.send(address_list, source)
 
 itchat.auto_login(hotReload=True)
